82a28088dc326748641b9ae0b219550d49929ed3
judif <jufreiberger@microsoft.com>
2022-08-18 10:38:06 +0200

    Secrets

--- dns/dns_private_resolver/providers.tf
+++ dns/dns_private_resolver/providers.tf
@@ -3,6 +3,5 @@ provider "azurerm" {
 
     tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
     subscription_id = "9f0a032d-81dc-4295-810f-21eb0327b0a1"
-    client_id       = "4292a611-d9a5-459f-abbb-bdb649543407"
-    client_secret   = "kWZ8Q~hw1Bf8B4F6NmgrP~ZnmX6LomWxWqdNfbpS"
+
 }
