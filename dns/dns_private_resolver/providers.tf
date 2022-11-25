ba11b9af1173b64cfb246ec4f7e9d9777804f5f8
Manuel Soelch <soelchma@gmail.com>
2022-11-25 21:07:10 +0100

    Fix cred

--- dns/dns_private_resolver/providers.tf
+++ dns/dns_private_resolver/providers.tf
@@ -4,5 +4,5 @@ provider "azurerm" {
     tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
     subscription_id = "c68e72e7-620a-4c1f-b3d8-f37845401a3f"
     client_id       = "fa2081f2-7eb4-4858-babf-f0822feab9f9"
-    client_secret   = "dAz8Q~eFZIyhqCKE-Li4LLQRg6rnsrYPmkjlkacK"
+    client_secret   = ""
 }
