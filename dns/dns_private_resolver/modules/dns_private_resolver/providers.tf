03e6309bcdc1c187e89118fecd7964324e6d880f
judif <jufreiberger@microsoft.com>
2022-12-01 16:49:29 +0100

    RM S

--- dns/dns_private_resolver/modules/dns_private_resolver/providers.tf
+++ dns/dns_private_resolver/modules/dns_private_resolver/providers.tf
@@ -9,8 +9,8 @@ terraform {
 provider "azurerm" {
     features {}
 
-    tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-    subscription_id = "9f0a032d-81dc-4295-810f-21eb0327b0a1"
-    client_id       = "4292a611-d9a5-459f-abbb-bdb649543407"
-    client_secret   = "mWb8Q~BDceqbvewVHLdQ_lAIInwzxa0RoSsqva2J"
+    tenant_id       = ""
+    subscription_id = ""
+    client_id       = ""
+    client_secret   = ""
 }
