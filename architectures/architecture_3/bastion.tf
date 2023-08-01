57aa8d23e00adfb72117c50f7451a6e8b22da5c1
Manuel Kutscher <kutscherma@outlook.com>
2023-08-01 16:58:54 +0200

    fix bastion bug

--- architectures/architecture_3/bastion.tf
+++ architectures/architecture_3/bastion.tf
@@ -1,17 +1,21 @@
 # Public IP Address
 resource "azurerm_public_ip" "bastion" {
   name                = "pip-bastion"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   allocation_method   = "Static"
 }
 
 
 # Azure Bastion Host
 resource "azurerm_bastion_host" "bastion" {
-  name                = "bastion"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  public_ip_address_id = azurerm_public_ip.bastion.id
-  virtual_network_subnet_id = azure_subnet.subnet_bastion_host.id
+  name                = "bastion-host"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.subnet_bastion_host.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
 }
