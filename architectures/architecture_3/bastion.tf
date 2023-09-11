bc52d22af23145fb17018739fe5816192d02af5c
Manuel Kutscher <kutscherma@outlook.com>
2023-09-11 10:18:57 +0200

    Fix ip address sku bastion host

--- architectures/architecture_3/bastion.tf
+++ architectures/architecture_3/bastion.tf
@@ -4,6 +4,7 @@ resource "azurerm_public_ip" "bastion" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 
